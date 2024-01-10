9845ad91e9ec7d58ed9c97ef0e0e01626f549ce7
Shreeti Kag <102422874+000626574705@users.noreply.github.com>
2024-01-10 11:56:15 +0530

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const DB_URL =process.env.DB_URL;
 const dbConnect = async () => {
   try {
-    await mongoose.connect('mongodb+srv://shreetikagmm123:shha161729@cluster0.jfmrdce.mongodb.net/?retryWrites=true&w=majority', {
+    await mongoose.connect('you mongodb url', {
       dbName: "mini-loan-app",
     });
     console.log("db connected");
